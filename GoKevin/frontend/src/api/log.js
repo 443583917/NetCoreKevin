77109d078dc74992c4c01def8575bf650880f3d8
夏之 <123313343+443583917@users.noreply.github.com>
2026-06-07 14:26:04 +0800

    2

--- GoKevin/frontend/src/api/log.js
+++
@@ -1,9 +0,0 @@
-import http from '../utils/http';
-
-export const GetHttpLogPageData = (params) => {
-  return http.post('/api/Log/GetHttpLogPageData', params);
-};
-
-export const GetOSLogPageData = (params) => {
-  return http.post('/api/Log/GetOSLogPageData', params);
-};
